d9913407fe345893d6703d1912b8e77865368df0
DanielJairton <daniel.jairton679@gmail.com>
2024-01-16 09:39:54 -0300

    update

---
+++ quiz/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Quizz from './quizz';
+
+function App() {
+  return (
+    <>
+    <Quizz>
+
+    </Quizz>
+    </>
+  );
+}
+
+export default App;
